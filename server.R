9450c9bfec849592befef418e5c5c9e443e3a693
avilesd <diego8fcb@hotmail.com>
2017-02-25 17:36:44 +0000

    Restrucutre App and add final details

--- server.R
+++ server.R
@@ -7,8 +7,10 @@ library(httr)
 # Import functions from other files if needed
 #source("locationToNumberOfBikes.R"
 
+
 # Load rentalZones for lat and lon data
-rentalZones <- read.csv2("/home/ubuntu/RENTAL_ZONE.csv", sep=";")
+
+rentalZones <- read.csv2("/srv/shiny-server/RENTAL_ZONE.csv", sep=";")
 stations <- rentalZones[!is.na(rentalZones$RENTAL_ZONE_X_COORDINATE),]
 stations <- stations[stations$RENTAL_ZONE_X_COORDINATE!=0,]
 
@@ -16,13 +18,14 @@ stations <- stations[stations$RENTAL_ZONE_X_COORDINATE!=0,]
 shinyServer(function(input, output) {
   
   pasteHelp <- function(street, city) {
-    anfangsort <- paste(input$strasse, input$stadt, "Germany", sep = ",")
-    anfangsort
+    anfangsort <- paste(street, city, "Germany", sep = ",")
+    return(anfangsort)
   }
+  print("Input DatePicker")
   
   # Listen for 'Button Click Event' and trigger the creation of the leaflet map
   # Also make sure to catch warnings and errors
-
+  
   observeEvent(input$showApp, {
     output$mymap <- renderLeaflet({
       result = tryCatch({
@@ -37,16 +40,67 @@ shinyServer(function(input, output) {
       }, finally = {
         
       })
-      # Rshiny function to use user-friendly error messages
-      validate(
+      shiny::validate(
         need(result, "Die Eingabe 'Anfangsort' ist nicht gültig. Bitte benutzen Sie folgendes Format: Strassenname Nr., Stadt")
       )
-      
       # Run only if input from 'Anfgansort' is valid, i.e. if geocode(...) was successful
       if (result){
-
+        #Lese SVMSpace ein      
+        SVMspace <- read.csv("/srv/shiny-server/StuttgartSVM.csv", sep=",", stringsAsFactors = FALSE, fileEncoding="UTF-8")
+        
+        rownames(SVMspace) <- SVMspace$X
+        SVMspace <-  SVMspace[colnames(SVMspace)!="X"]
+        SVMspace <- SVMspace[colnames(SVMspace)!="hour"]
+        #Lege einen Data Frame an für die Klassen
+        categories  <-  as.data.frame(SVMspace[,"category"])
+        colnames(categories) <- "category"
+        #Lösche im SVMRaum das Klassenattribut
+        SVMspace <- SVMspace[colnames(SVMspace)!="category"]
+        #Lösche Schnee, da er kaum auswirkungen hat
+        SVMspace <- SVMspace[colnames(SVMspace)!="SNWD"]
+        
+        #Lese die Veränderungen pro klasse aus
+        deltaClass <- read.csv("/srv/shiny-server/StuttgartVeränderungKlassen.csv", sep=",", stringsAsFactors = FALSE, fileEncoding="UTF-8")
+        deltaClass <- deltaClass[names(deltaClass)!="X"]
+        colnames(deltaClass)[1:(ncol(deltaClass)-1)] <- substring(colnames(deltaClass)[1:(ncol(deltaClass)-1)],2)
+        colnames(deltaClass) <-gsub("\\.", " ", colnames(deltaClass))
+        
+        #Erzeuge url zum Abruf von Wetterdaten
+        url <- "http://api.openweathermap.org/data/2.5/forecast?units=metric&appid=a68fd0d9af55f756ea35eb3f51a4c3b6&q="
+        url <- paste(url,"Stuttgart",sep="") 
+        
+        # Formatiere die Wetterdaten entsprechend
+        # rufe hierbei Wetterdaten als JSON Ojekt ab
+        getContent <- fromJSON(url) # content function will try to parse to R object
+        tmax <- getContent$list$main$temp_max
+        tmin <- getContent$list$main$temp_min
+        tavg <- (tmax-tmin)/2+tmin
+        remove(weatherData)
+        weatherData <- data.frame(matrix(0, ncol = 7,nrow=nrow(getContent$list)))
+        weatherData$X1 <- as.POSIXct(getContent$list$dt_txt)
+        weatherData$X2 <- as.numeric(strftime(weatherData$X1, "%H"))
+        weatherData$X3 <- tmax
+        weatherData$X4 <- tmin
+        weatherData$X5 <- tavg
+        if(length(getContent$list$rain$`3h`)!=0){
+          weatherData$X6 <- getContent$list$rain$`3h`*8
+        }else{
+          weatherData$X6 <- 0
+        }
+        weatherData$X7 <- as.numeric(strftime(weatherData$X1,"%w"))
+        colnames(weatherData)[1] <- "Date"
+        colnames(weatherData)[2] <- "hour"
+        colnames(weatherData)[3:ncol(weatherData)] <- colnames(SVMspace)
+        weatherData$PRCP[is.na(weatherData$PRCP)] <- 0
+        
+        #Erstelle SVM-Model auf Basis der historischen Daten
+        svm.model <- svm(SVMspace, categories[,], kernel="linear")
+        #Klassifiziere aktuelle Wetterdaten
+        svm.pred <-  predict(svm.model, weatherData[,3:ncol(weatherData)])
         lon <- lonLat[1]
         lat <- lonLat[2]
+        print(lon)
+        print(lat)
         
         tripleTable <- triangulateLocation(lat, lon, Sys.time(), input$radius)
         
@@ -54,31 +108,63 @@ shinyServer(function(input, output) {
         finalLat  <- as.numeric(as.vector(tripleTable[, 1])) #resultList$finalLat
         finalLon  <- as.numeric(as.vector(tripleTable[, 2])) #resultList$finalLon
         finalFreq <- as.numeric(as.vector(tripleTable[, 3])) #resultList$finalFreq
-        
+        futureFreq <- as.numeric(as.vector(tripleTable[, 3])) #Vector fututre Freq
+        reserveTagLat <-vector() #Vector Reserve Tag per Station Lat
+        reserveTagLon <-vector() #Vector Reserve Tag per Station Lon
+        tag <- FALSE
         # We use 'leafletProxy' to change the Layers on the map without rerendering
         if(input$showCurrent) {
           leafletProxy("mymap") %>%
-            clearShapes() %>%
-            addCircleMarkers(finalLon, finalLat, radius = finalFreq*1.7,
+            addCircleMarkers(finalLon, finalLat, radius = finalFreq*3,
                              popup = paste(as.character(finalFreq), " Fahrräder(aktuell)",
                                            sep=""), color = c('red'))
         }
         else {
+          #Berechne die Vorhersage für die verschiednenen Stationen
+          for (i in 1:length(finalLat)){
+            deltaPerStationHourly <- getDelta(finalLat[i],finalLon[i], input$datePicker, weatherData, svm.pred, deltaClass)
+            futureFreqActual <-  futureFreq[i]+sum(deltaPerStationHourly)
+            if(futureFreqActual<0){futureFreqActual <- 0}
+            futureFreq[i] <- futureFreqActual
+            costNotToReserveStation <- costNotToReserve(futureFreqActual)
+            costReserveStation <- costReserve(finalFreq[i],deltaPerStationHourly)
+            if(input$costRatio*costNotToReserveStation>costReserveStation){
+              tag <- TRUE
+              reserveTagLon <- c(reserveTagLon,finalLon[i])
+              reserveTagLat <- c(reserveTagLat,finalLat[i])
+            }
+          }
           print("Unchecked")
         }
         
         #
         # leaflet() has to be the last operation!!!
-        #
-        
+        #Füge entsprechende Marker für Leaflet hinzu
         logoPath = 'location-pointer.png'
-        leaflet(data = stations[stations$CITY==input$stadt,]) %>% addTiles() %>%
-          addMarkers(as.numeric(lon) , as.numeric(lat), icon = list(
-            iconUrl = logoPath, iconSize = c(30, 50)
-          )) %>%
-          addMarkers(~RENTAL_ZONE_X_COORDINATE, ~RENTAL_ZONE_Y_COORDINATE,popup = 2) %>%
-          setView(lat, lon, zoom = 17)
         
+        if(tag){
+          leaflet(data = stations[stations$CITY==input$stadt,]) %>% addTiles() %>%
+            addMarkers(as.numeric(lon) , as.numeric(lat), icon = list(
+              iconUrl = logoPath, iconSize = c(30, 50)
+            )) %>%
+            addMarkers(~RENTAL_ZONE_X_COORDINATE, ~RENTAL_ZONE_Y_COORDINATE,popup = ~as.character(RENTAL_ZONE_GROUP)) %>%
+            addCircleMarkers(finalLon, finalLat, radius = futureFreq*3,
+                             popup = paste(as.character(round(futureFreq)), " Fahrräder (Prognose)",
+                                           sep=""), color = c('blue')) %>%
+            addPopups(reserveTagLon, reserveTagLat, "Hier Reservieren (Empfehlung)") %>%
+            setView(lat, lon, zoom = 17)
+          
+        }else{
+          leaflet(data = stations[stations$CITY==input$stadt,]) %>% addTiles() %>%
+            addMarkers(as.numeric(lon) , as.numeric(lat), icon = list(
+              iconUrl = logoPath, iconSize = c(30, 50)
+            )) %>%
+            addMarkers(~RENTAL_ZONE_X_COORDINATE, ~RENTAL_ZONE_Y_COORDINATE,popup = ~as.character(RENTAL_ZONE_GROUP)) %>%
+            addCircleMarkers(finalLon, finalLat, radius = futureFreq*3,
+                             popup = paste(as.character(round(futureFreq)), " Fahrräder (Prognose)",
+                                           sep=""), color = c('blue')) %>%
+            setView(lat, lon, zoom = 17)
+        }
       }
       
     })
@@ -114,15 +200,15 @@ shinyServer(function(input, output) {
 
 locationToBikes <- function(latitude, longitude, inputTime, radius) {
   # Key used in application is stored locally for security purposes
-  apikey <- Sys.getenv("CALLABIKE_API")
-  customRadius <- radius
+  apikey <- "Bearer 97c4f3365b57d92966e97e92e02c4eb4"
+  customRadius <- 500#radius
   customLimit <- 50
   begin <- as.character(inputTime)
   
   providerNetwork <- 2 # tell API to use Call-a-bike Network
   
   getResults <- GET("https://api.deutschebahn.com/flinkster-api-ng/v1/bookingproposals", 
-                    query = list(lat = latitude, lon = longitude, radius= customRadius, begin=begin,
+                    query = list(lat = latitude, lon = longitude, radius= customRadius,
                                  limit = customLimit, providernetwork = providerNetwork),
                     , add_headers(Accept = "application/json", Authorization = apikey))
   # Get parsed JSON content
@@ -141,10 +227,9 @@ locationToBikes <- function(latitude, longitude, inputTime, radius) {
 }
 
 triangulateLocation <- function (latitude, longitude, dateTime, radius, merge = TRUE) {
-  print(dateTime)
-  print(as.Date(dateTime))
+  attr(dateTime, "tzone") <- "UTC"
   dateTime <- gsub(" ", "T", dateTime)
-  dateTime <- paste(dateTime, "-01:00:00", sep="")
+  dateTime <- paste(dateTime, "-00:00:00", sep="")
   print(dateTime)
   # Call locationBikes with original coordinates
   mainLocation <- locationToBikes(latitude, longitude, dateTime, radius)
@@ -171,4 +256,76 @@ triangulateLocation <- function (latitude, longitude, dateTime, radius, merge = 
     df4$"newFreq" <- newCol
   }
   df4
+}
+
+#Lade benötigte Pakete für SVM und JSON umwandlung der Wetterdaten
+require(e1071)
+library(jsonlite)
+
+#Funktion um die Veränderung an einer Station zu bekommen
+getDelta <- function (lat,lon,futureTime, weatherData, svm.pred, deltaClass, resultType = "delta"){
+  #Finde Stationennamen 
+  stationName <-  as.character(stations[which.min(abs((stations$RENTAL_ZONE_X_COORDINATE-lat)+abs(stations$RENTAL_ZONE_Y_COORDINATE-lon))),]$RENTAL_ZONE_GROUP)
+  stationName <-gsub("-", " ", stationName)
+  #Wandle Zeit in entsprechendes Format
+  futureTime <- as.POSIXct(futureTime)
+  #Speicher relevante Wetterdaten (kleiner als die Zeit in der Zukunft)
+  forecast <- weatherData[weatherData$Date<=futureTime,]
+  #Wie viele Stunden sollen Vorhergesagt werden
+  currentTime <- Sys.time()
+  attr(currentTime, "tzone") <- "Europe/Paris"
+  hours <- round(as.numeric(difftime(futureTime,currentTime,units="hours")))
+  
+  #Speichere die gesamte Veränderung der Station
+  stationChange <- 0
+  for(i in 1:hours)
+  {
+    currentHour <- currentTime+i*60*60
+    if(length(rownames(forecast[forecast$Date<=currentHour,]))==0){
+      forecastIndex <- 1
+    }else{
+      forecastIndex <- max(as.numeric(rownames(forecast[forecast$Date<=currentHour,])))
+    }
+    classHour <- as.matrix(c(svm.pred))[forecastIndex]
+    hourOfDay <- as.numeric(strftime(currentHour, "%H"))+1
+    
+    stationChange[i] <- deltaClass[deltaClass$class==classHour,colnames(deltaClass)==stationName][hourOfDay]
+  }
+  #Gebe Veränderung an Station zurück
+  return(stationChange)
+}
+
+costNotToReserve <- function (finalProjection) {
+  # Assure that no negative final projection is allowed
+  costs <- 1/(1+finalProjection)
+  return(costs)
+}
+
+costReserve <- function(currentBikes, projectedChange, reserved=1) {
+  # You have to account for the bikes that are reserved
+  totalNumberBikes <- currentBikes + reserved
+  totalCosts <- 0
+  # Begin algorithm
+  for (j in 1:length(projectedChange)) {
+    if(projectedChange[j] >= 0) {
+      cost <- 0
+    }
+    else {
+      cost <- (reserved/totalNumberBikes)*abs(projectedChange[j]) 
+    }
+    totalCosts <- totalCosts + cost
+    currentBikes <- turnXIfLessThanX(currentBikes + projectedChange[j], 0)
+    totalNumberBikes <- turnXIfLessThanX(totalNumberBikes + projectedChange[j], 1)
+  }
+  totalCosts
+}
+# Auxiliary function for turning values less than 0 automatically into 0
+turnXIfLessThanX <- function(actualValue, x) {
+  if (actualValue <= x) {
+    result <- x
+  }
+  else {
+    result <- actualValue
+  }
+  result
 }
--- ui.R
+++ ui.R
@@ -1,5 +1,8 @@
+library(shiny)
+library(shinyjs)
 library(leaflet)
-#library(shinyjs)
+library(ggmap)
+library(httr)
 shinyUI(fluidPage(
   #useShinyjs(),  # Include shinyjs
   
@@ -14,29 +17,30 @@ shinyUI(fluidPage(
   ),
   
   HTML("<script type='text/javascript'>
-        $( document ).ready(function() {
-         $('#datePicker').datetimepicker({format: 'yyyy-mm-dd hh:ii', endDate: '+4d',
-        autoclose: true});
-        });
+       $( document ).ready(function() {
+       $('#datePicker').datetimepicker({format: 'yyyy-mm-dd hh:ii', endDate: '+4d',
+       startDate:'+0d', autoclose: true});
+       $('#datePicker').attr('readonly','');
+       });
        </script>"),
   
   # Navigation Bar
   HTML("<nav class='navbar navbar-inverse navbar-fixed-top' role='navigation'>
-      <div class=''>
-        <div class='navbar-header'>
-          <button type='button' class='navbar-toggle collapsed' data-toggle='collapse' data-target='#navbar' aria-expanded='false' aria-controls='navbar'>
+       <div class=''>
+       <div class='navbar-header'>
+       <button type='button' class='navbar-toggle collapsed' data-toggle='collapse' data-target='#navbar' aria-expanded='false' aria-controls='navbar'>
        <span class='sr-only'>Toggle navigation</span>
-         <span class='icon-bar'></span>
-         <span class='icon-bar'></span>
-         <span class='icon-bar'></span>
-         </button>
-         <a class='navbar-brand' href='#'>Book a Bike</a>
-         </div>
-         <div id='navbar' class='navbar-collapse collapse'>
-         
-         </div><!--/.navbar-collapse -->
-         </div>
-         </nav>"),
+       <span class='icon-bar'></span>
+       <span class='icon-bar'></span>
+       <span class='icon-bar'></span>
+       </button>
+       <a class='navbar-brand' href='#'>Book a Bike</a>
+       </div>
+       <div id='navbar' class='navbar-collapse collapse'>
+       
+       </div><!--/.navbar-collapse -->
+       </div>
+       </nav>"),
   
   HTML("<legend style='padding-top: 5px;'>Reiseauskunft</legend>"),
   
@@ -45,34 +49,35 @@ shinyUI(fluidPage(
   #
   
   tags$div(id="removeDiv",
-    fluidRow(
-      column(6, 
-        textInput("strasse", "Strasse & Strassennr.", value = "", placeholder = "Koenigsstrasse 1")
-      ),
-      column(6, 
-             selectInput("stadt", "Stadt", c("Stuttgart" = "Stuttgart", "Hamburg" = "Hamburg"), selected = "Stuttgart")
-             #textInput("stadt", "Stadt", value = "", placeholder = " Stuttgart")
-      ),
-      column(12,
-        textInput("datePicker", "Datum & Uhrzeit", value = as.character(Sys.time())),
-        
-        actionButton("showApp", "Suchen", class="pull-right"),
-        leafletOutput("mymap")
-      )
-    )
+           fluidRow(
+             column(6, 
+                    textInput("strasse", "Standort: Straße & Straßennr.", value = "", placeholder = "z.B. Königstraße 1")
+             ),
+             column(6, 
+                    selectInput("stadt", "Stadt", c("Stuttgart" = "Stuttgart"), selected = "Stuttgart")
+                    #textInput("stadt", "Stadt", value = "", placeholder = " Stuttgart")
+             ),
+             column(12,
+                    textInput("datePicker", "Datum & Uhrzeit", value = as.character(Sys.time())),
+                    actionButton("showApp", "Suchen", class="pull-right"),
+                    leafletOutput("mymap")
+             )
+           )
   ),
   
   tags$div(id="parameters",
-    HTML("<div class='col-xs-12 parameters-top'>"), 
-      HTML("<div class='col-xs-12 col-sm-3'>"), 
-        checkboxInput("showCurrent", "Aktuell", value = FALSE),
-      HTML("</div>"),
-      HTML("<div class='col-xs-12 col-sm-3'>"), 
-        textInput("radius", "Radius", value = 700),
-      HTML("</div>"),
-    HTML("</div>") 
+           HTML("<div class='col-xs-12 parameters-top'>"), 
+           HTML("<div class='col-xs-12 col-sm-2'>"), 
+           checkboxInput("showCurrent", "Aktuell", value = FALSE),
+           HTML("</div>"),
+           HTML("<div class='col-xs-12 col-sm-5'>"), 
+           sliderInput("radius", "Vorhersageradius in Meter", min=200, max=8200, value=800),
+           HTML("</div>"),
+           HTML("<div class='col-xs-12 col-sm-5'>"), 
+           sliderInput("costRatio", "Kostenverhältnis/Risikoaversion", min=0.1, max=5, value=1.8),
+           HTML("</div>"),
+           HTML("</div>")
   ),
-  
   tags$footer(id="footer", tags$p("© FZI Seminar - Gruppe 3"))
   
-))
+  ))
